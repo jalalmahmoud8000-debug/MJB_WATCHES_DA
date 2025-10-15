2da2429815a5b958e8a0229d8882bb51724d410e
jalalmahmoud8000 <jalalmahmoud8000@gmail.com>
2025-10-15 01:02:49 +0000

    debug fix

--- mysite/pages/admin.py
+++ mysite/pages/admin.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
-from .models import ContactMessage
+from .models import Contact
 
-@admin.register(ContactMessage)
-class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'name', 'email', 'created_at')
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subject', 'created_at')
+    search_fields = ('email', 'subject', 'message')
     readonly_fields = ('name', 'email', 'subject', 'message', 'created_at')
-    search_fields = ('subject', 'name', 'email')
